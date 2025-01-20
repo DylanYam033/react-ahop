914f5c71a8ce0b850b532b2ddfaee5126016c6c6
DylanYam033 <dylan.yampuezan@wactechnology.com>
2025-01-20 13:43:35 -0500

    ajuste vite config to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // Asegúrate de que sea compatible con top-level await
+  },
 })
